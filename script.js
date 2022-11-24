8f9b6973e4356d57eb31f3e6fdc60eeb3dea70f2
ANUAR <anuarprogramm@gmail.com>
2022-11-24 22:02:47 +0600

    add png

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let $start = document.querySelector('#start')
 let $startFrame = document.querySelector('.start')
 let $game = document.querySelector('.game')
 let $table = document.querySelector('#table')
+let $circle = document.querySelector("#circle")
+let level = 0
 var listener = function(e) {
   div.style.left = e.pageX - 50 + "px";
   div.style.top = e.pageY - 50 + "px";
@@ -18,12 +20,21 @@ circle.addEventListener('mouseup', e => {
     let divy = div.getBoundingClientRect().y
     let tablex = $table.getBoundingClientRect().x
     let tabley = $table.getBoundingClientRect().y
-    // if (){
-    //     alert("you won")
-    // }
+    if (Math.abs(divx-tablex)<=80   && Math.abs(divy-tabley) <= 50){
+        alert("you won")
+        level++
+        levels(level)
+    }
 });
 
 $start.addEventListener('click', function(){
     $startFrame.classList.add('hide')
     $game.classList.remove('hide')
 })
+
+function levels(level){
+    $circle.style.top = 0
+    $circle.style.left = 0
+    $table.style.backgroundImage = `url(./table${level}.png)`
+    $circle.style.backgroundImage = `url(./ball${level}.png)`
+}
